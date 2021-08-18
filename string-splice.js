3883c1d18dcc17d2a3eda54c00d8ac52daf675e0
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-19 00:13:15 +0600

    nothing

--- string-splice.js
+++ string-splice.js
@@ -2,18 +2,18 @@ const anthem = "Amr shunar bangla ami tomay valobashi"
 
 // Split()
 const words = anthem.split(" ") // it means without space . prottekta ke alada kore dekhabe
-// console.log(words);
+console.log(words);
 
 
 // Slice()
 const slicing = anthem.slice( /* started form index (4) */ 4,/* ends before index (11) */11)
-// console.log(slicing);
+console.log(slicing);
 
 
 
 // substr()
 const another = anthem.substr(4, 6) // selected index er por theke koyta index lagbe sheta bole dite hoy
-// console.log(another);
+console.log(another);
 
 
 
@@ -25,10 +25,10 @@ const another = anthem.substr(4, 6) // selected index er por theke koyta index l
 
 
 // Concat()
-// const firstName = "Shfayet"
-// const lastName = "Hossain"
-// const fullName = firstName.concat(" ").concat(lastName)
-// console.log(fullName);
+const firstName = "Shfayet"
+const lastName = "Hossain"
+const fullName = firstName.concat(" ").concat(lastName)
+console.log(fullName);
 
 
 
