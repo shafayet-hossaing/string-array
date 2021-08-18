b673b7d0d400828d3208e63648c67e3ed90f4cef
shafayet-hossain <shafayethossaing4609@gmail.com>
2021-08-18 23:07:56 +0600

    coded

--- case-convert.js
+++ case-convert.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const userInput = "blackPink"
+const savedUserName = "blAckPink"
+
+
+// Using toLowerCase, if needed can be used toUpperCase
+if(userInput.toLocaleLowerCase == savedUserName.toLocaleLowerCase){
+    console.log("Matched");
+}
